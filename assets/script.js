a537dd36e5e5bc54c61845e72d0d966f670f4fbd
H4M4IOS <165417992+H4M4IOS@users.noreply.github.com>
2024-03-29 23:46:22 +0300

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,24 @@
+document.getElementById("pubg-indiaButton").addEventListener("click", function() {
+    alert("BGMI 3.1.0                                                         Aim Bot 80m✅                                  No Ricoil✅                                                       Small Cross✅                                                       Withe Body✅                                                   No Camera Shake✅                                         Antiban VIP hack✅                                                Click Closeto download👇                                                     Wait 5 seconds and then you can download it5️⃣.......");
+    window.location.href = "https://drive.google.com/file/d/11o0Luzsif6WM-F8OWzkz9ys6mFZKjAco/view?usp=drivesdk";
+});
+
+document.getElementById("youtubeButton").addEventListener("click", function() {
+    alert("بەردەست نیە لە ئایندەیەکی نزیک بەردەست دەبێت");
+    window.location.href = "";
+});
+
+document.getElementById("googlePlayButton").addEventListener("click", function() {
+    alert("بەردەست نیە لە ئایندەیەکی نزیک بەردەست دەبێت");
+    window.location.href = "";
+});
+
+document.getElementById("facebookButton").addEventListener("click", function() {
+    alert("بەردەست نیە لە ئایندەیەکی نزیک بەردەست دەبێت");
+    window.location.href = "";
+});
+
+document.getElementById("instagramButton").addEventListener("click", function() {
+    alert("بەردەست نیە لە ئایندەیەکی نزیک بەردەست دەبێت");
+    window.location.href = "";
+});
