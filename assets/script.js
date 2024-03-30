881078b5795c274c839c20a4bca40ce2beead202
H4M4IOS <165417992+H4M4IOS@users.noreply.github.com>
2024-03-30 12:10:02 +0300

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,10 @@
 document.getElementById("pubg-globalButton").addEventListener("click", function() {
-    alert("BGMI 3.1.0                                                         Aim Bot 80m✅                                  No Ricoil✅                                                       Small Cross✅                                                       Withe Body✅                                                   No Camera Shake✅                                         Antiban VIP hack✅                                                Click Closeto download👇                                                     Wait 5 seconds and then you can download it5️⃣.......");
+    alert("BGMI 3.1.0                                                                                                      Aim Bot 80m✅                                                                                         No Ricoil✅                                                                                                                Small Cross✅                                                                                    Withe Body✅                                                                           No Camera Shake✅                                                                          Antiban VIP hack✅                                                               Click Close to download👇                                                     Wait 5 seconds and then you can download it5️⃣.......");
     window.location.href = "https://drive.google.com/file/d/11o0Luzsif6WM-F8OWzkz9ys6mFZKjAco/view?usp=drivesdk";
 });
 
 document.getElementById("pubg-koreButton").addEventListener("click", function() {
-    alert("KRJB 3.1                         Esp                                               Aimbot");
+    alert("KRJB 3.1                                                             Esp                                          Aimbot                                                        Click Close to download👇                                                     Wait 5 seconds and then you can download it5️⃣.......");
     window.location.href = "https://mega.nz/file/N60X1I7J#tGya3Un2p2SQvcIZ7JEMC2tixcJUwiPfXBE8wlTImPY";
 });
 
